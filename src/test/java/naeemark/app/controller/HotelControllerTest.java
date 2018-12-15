0cef1f7340752fd14616b0653545b601ce06097f
Naeem <naeemark@gmail.com>
2018-12-15 18:31:38 +0800

    + Adds Unit Tests

---
+++ src/test/java/naeemark/app/controller/HotelControllerTest.java
@@ -0,0 +1,28 @@
+package naeemark.app.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+/**
+ * Created by Naeem <naeemark@gmail.com>.
+ * <p>
+ * Created on: 2018-12-15
+ */
+@RunWith(SpringRunner.class)
+public class HotelControllerTest {
+
+    @Test
+    public void testGetHotels(){
+
+//        this.mockMvc.perform(MockMvcRequestBuilders.get("/hotels/a"))
+//                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_UTF8))
+//                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+}
--- src/test/java/naeemark/app/model/HotelTest.java
+++ src/test/java/naeemark/app/model/HotelTest.java
@@ -2,8 +2,11 @@ package naeemark.app.model;
 
 import naeemark.Address;
 import naeemark.Hotel;
+import org.assertj.core.api.Assertions;
+import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
 
@@ -14,15 +17,22 @@ import java.util.ArrayList;
  */
 public class HotelTest {
 
+    @Autowired
+    private Address address;
+
 
     @Test
     public void testCreation() {
 
-        Address address = new Address("KL", "ML");
-
         Hotel hotel = new Hotel("Taj", 100, address, new ArrayList<>());
         Assert.assertEquals("Taj", hotel.getName());
-        Assert.assertEquals("KL", hotel.getAddress().getCity());
+
+        Assertions.assertThat(hotel.getName()).isNotBlank();
+        Assert.assertEquals("Taj", hotel.getName());
+        Assert.assertThat(hotel.getName(), Matchers.equalTo("Taj"));
+        Assertions.assertThat(hotel.getName()).isEqualTo("Taj");
+
+        Assertions.assertThat(hotel.getPricePerNight()).isGreaterThan(0);
 
     }
 }
